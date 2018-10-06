e3207e7a9ec16c605ec5852bf6b323823e801a0a
M.Larkowski U1864886 <U1864886@unimail.hud.ac.uk>
2018-10-06 23:53:20 +0100

    first commit

---
+++ Week 2 Practical/hi.py
@@ -0,0 +1,9 @@
+number_of_sweets = 47
+number_of_pupils = -16
+
+sweets_per_pupil = number_of_sweets // number_of_pupils
+
+left_over = number_of_sweets % number_of_pupils
+
+print('Sweets per pupul', sweets_per_pupil,
+      'Sweets left for the teacher', left_over)
