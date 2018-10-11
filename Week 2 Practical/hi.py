b3aaf9e04e344386a8f6fd2efa3d85e3284785e0
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-10-11 13:43:18 +0100

    big update

--- Week 2 Practical/hi.py
+++ Week 2 Practical/hi.py
@@ -1,9 +1,21 @@
-number_of_sweets = 47
-number_of_pupils = -16
 
-sweets_per_pupil = number_of_sweets // number_of_pupils
+try:
+    number_of_pupils = int(input('Provide number of pupils: '))
 
-left_over = number_of_sweets % number_of_pupils
+    number_of_sweets = int(input('Provide number of sweets: '))
 
-print('Sweets per pupul', sweets_per_pupil,
-      'Sweets left for the teacher', left_over)
+    if number_of_pupils > 0 and number_of_sweets > 0:
+        sweets_per_pupil = number_of_sweets // number_of_pupils
+
+        left_over = number_of_sweets % number_of_pupils
+
+
+        print('Sweets per pupil', sweets_per_pupil,'\nSweets left for the teacher', left_over)
+    elif number_of_pupils == 0:
+        print('There are no pupil to share the sweets between.\n Sweets left for the Teacher:', number_of_sweets)
+    elif number_of_sweets == 0:
+        print('There are no sweets to share')
+    else:
+        print('negative integer')
+except ValueError:
+    print('Enter integer')
