9c9eadbee00adaa11572bca61e123f77c5e57dca
LarryOG <lary.larkowski@gmail.com>
2018-10-18 17:05:01 +0100

    first commit pycharm K drive

---
+++ Week 4 Practical/fiveitems.py
@@ -0,0 +1,10 @@
+
+
+price1 = input('Enter the price of the first sweet: ')
+price2 = input('Enter the price of the second sweet: ')
+price3 = input('Enter the price of the third sweet: ')
+price4 = input('Enter the price of the fourth sweet: ')
+price5 = input('Enter the price of the fifth sweet: ')
+
+total_prices = int(price1 [:-1]) + int(price2 [:-1]) + int(price3 [:-1]) + int(price4 [:-1]) + int(price5 [:-1])
+print (total_prices)
--- Week 4 Practical/hello Sir.py
+++ Week 4 Practical/hello Sir.py
@@ -2,4 +2,4 @@ name = input('Hello, what\'s your name? \n')
 if len(name) == 0:
     print('Hello, World')
 else:
-    print('Hello,  '+ name.capitalize()+'.', '\nIt\'s nice to meet you.')
+    print('Hello, Sir '+ name.capitalize()+'.', '\nIt\'s nice to meet you.')
