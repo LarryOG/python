7b0eec9120cd3d8e213497fcff4521ce7ccd03bf
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-10-25 11:13:34 +0100

    small change

--- Week 4 Practical/fiveitems.py
+++ Week 4 Practical/fiveitems.py
@@ -6,8 +6,8 @@ price3 = input('Enter the price of the third sweet: ')
 price4 = input('Enter the price of the fourth sweet: ')
 price5 = input('Enter the price of the fifth sweet: ')
 prices_all = (int(price1 [:-1]),int(price2 [:-1]),int(price3 [:-1]),int(price4 [:-1]),int(price5 [:-1]))
-total_prices = int(price1 [:-1]) + int(price2 [:-1]) + int(price3 [:-1]) + int(price4 [:-1]) + int(price5 [:-1])
-average_price =(int(price1 [:-1]) + int(price2 [:-1]) + int(price3 [:-1]) + int(price4 [:-1]) + int(price5 [:-1]))/5
+total_prices = sum(prices_all)
+average_price = total_prices/len(prices_all)
 highest_price = max(prices_all)
 lowest_price = min(prices_all)
 
