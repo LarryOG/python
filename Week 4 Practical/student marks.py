f2a78b355dd531daba21b6935eb792d17f17de49
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-10-25 15:59:07 +0100

    something something. loops etc

--- Week 4 Practical/student marks.py
+++ Week 4 Practical/student marks.py
@@ -1,13 +1,18 @@
 name = input('Enter the studnet\'s name:')
-
-total_marks = 0
+NUMBER_OF_MARKS = 5
+total_marks = []
 try:
 
-    for i in range (5):
+    for i in range (NUMBER_OF_MARKS):
         mark = int(input('Enter student\'s mark: '))
-        total_marks += mark
+        if mark in range (0,101):
+            total_marks.append(mark)
 
-    average_mark = total_marks / 5
+        else:
+         print ('Please input correct mark.')
+         raise SystemExit
+
+    average_mark = sum(total_marks) / NUMBER_OF_MARKS
 
     print ('Final Mark for ' + name.capitalize() + ' is ' + str(average_mark))
 
