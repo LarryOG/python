e7964704d05e1d188a1112d651b9c409e475e210
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-11-08 22:54:17 +0000

    decimal converter final. premier league working, can use bit of refactoring.

--- Week 6 Practical/Premier League statistics calculator.py
+++ Week 6 Practical/Premier League statistics calculator.py
@@ -3,10 +3,25 @@ Number_of_Results = 5
 
 goals_scored_in_games = []
 goals_against_in_games = []
-
-for count in range (Number_of_Results):
+try:
+    for count in range (Number_of_Results):
     goals_scored = int(input('Goals Scored in Match #'+ str(count + 1)+': '))
     goals_scored_in_games.append (goals_scored)
     goals_against = int(input('Goals Against in Match #'+ str(count + 1)+': '))
     goals_against_in_games.append (goals_against)
+    wins = 0
+    draws = 0
+    loses = 0
+    points = 0
+except ValueError:
+    print('Error. Enter an integer.')
+for i in range (Number_of_Results):
+    if goals_against_in_games[i] < goals_scored_in_games[i]:
+     wins += 1
+    elif goals_against_in_games[i] > goals_scored_in_games[i]:
+     loses += 1
+    elif goals_scored_in_games[i] == goals_against_in_games[i]:
+     draws += 1
+points =  wins * 3 + draws
+print(Team_Name + ': ','\nWins: ', wins,'\nDraws: ', draws, '\nLoses: ',loses,'\nPoints: ',points)
 
---
+++ Week 6 Practical/decimal to binary.py
@@ -0,0 +1,3 @@
+
+for x in range (0,128):
+    print( x,'\t',"{0:08b}".format(x))
