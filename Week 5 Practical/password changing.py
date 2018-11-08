47bc7fd9ee28c2c607894c07cbe63839e1b5f6b3
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-11-08 14:26:01 +0000

    password changing program final

--- Week 5 Practical/password changing.py
+++ Week 5 Practical/password changing.py
@@ -1,13 +1,25 @@
 username = input('Enter your username: \n')
 student_ID = input('Enter your student ID: \n')
 
-password = input('Enter password: \n')
-password2 = input('Enter password again: \n')
+while 1:
+    password = input('Enter password: \n')
+    banned_words = [username,student_ID,'huddersfield','justinbieber','cheese','canalside']
 
-banned_words = [username,student_ID,'huddersfield','justinbieber','cheese','canalside']
+    if len(password) in range (6,13):
+        if password in banned_words:
+            print('Password too simple. Try different one.')
+        else:
+            password2 = input('Enter password again: \n')
+            if password != password2:
+                print ('Passwords do not match. Please try again.')
+            else:
+                print ('Password changed.')
+                break
+    else:
+        print('Password lenght is invalid. Please try again.')
 
-if password == password2:
-    if len(password)>5 and len(password)<=13:
-         if password in banned_words:
-             print('You need to use different password')
-         print('Password Changed')
+
+
+
+
+
